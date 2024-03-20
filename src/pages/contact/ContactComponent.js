293890c493c987d7463c25fc9d5cd72dc7d2c184
kristiankyvik <kristianruizkyvik@gmail.com>
2024-03-20 22:45:25 +0100

    sa

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -51,7 +51,7 @@ function Contact(props) {
               <br />
               <br />
               <a {...styles} className="general-btn" href={greeting.resumeLink}>
-                aasdasdassd
+                wasdsa
               </a>
             </div>
           </div>
