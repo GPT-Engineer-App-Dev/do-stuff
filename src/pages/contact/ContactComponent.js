c38f1a03a889017125f852d3f7e03b5c3f92f366
kristiankyvik <kristianruizkyvik@gmail.com>
2024-03-21 11:53:11 +0100

    referes commit

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -71,7 +71,7 @@ function Contact(props) {
               </p>
               <div className="blogsite-btn-div">
                 <a {...styles} className="general-btn" href={blogSection.link}>
-                  My dsaads
+                  My sdaasd
                 </a>
               </div>
             </div>
